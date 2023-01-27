112e01a072362d577dcc7fc153a8aa06ab69b9db
joel <joel_mpy@hotmail.com>
2023-01-27 12:21:19 +0100

    nav-bar reponsive end

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 // let text = document.querySelector(".first-text")
-// text.innerHTML = "bonjour a tous ici javascript"
+// text.innerHTML = "bonjour a tous ici javascript"*
+
+let body = document.querySelector('body')
+let toggle = document.querySelector('.toggle')
+
+toggle.addEventListener('click', function(){
+    body.classList.toggle('open')
+})
